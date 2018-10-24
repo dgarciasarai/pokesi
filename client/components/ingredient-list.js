77c12951e3b2e4a1d29fc659ff0a17f7901dabaf
Sarai Díaz García <dgarciasarai@gmail.com>
2018-10-24 10:21:40 +0200

    THE font

--- client/components/ingredient-list.js
+++ client/components/ingredient-list.js
@@ -7,13 +7,13 @@ function IngredientList (state, actions) {
 
   return (
     <div>
-      <div class="header">POKESI</div>
+      <div class="header"><span>POKE</span><span class="header-si">SI</span></div>
       <div class="ingredients">
         <p>Selecciona tus ingredientes</p>
         <ul class="ingredients-list">{state.ingredients.map(ingredient =>
-            <li class="ingredient-element">
+            <li class="ingredient-item">
               <img class="ingredient-image" src={ingredient.image}/>
-              {ingredient.name}
+              <span class="ingredient-name">{ingredient.name}</span>
             </li>
           )}</ul>
       </div>
