d3d72e2e0dee9589dbbba25a99569dccd7e2da5d
Pablo Molina <contact@pablomolina.me>
2018-10-25 09:02:55 +0200

    Include popstate navigation

--- client/components/app.js
+++ client/components/app.js
@@ -2,6 +2,8 @@ import IngredientDetail from './ingredient-detail'
 import IngredientList from './ingredient-list'
 import Summary from './summary'
 
+let locationListener;
+
 function app(state, actions) {
   const pathname = state.route.pathname
   let view = IngredientList
@@ -10,6 +12,11 @@ function app(state, actions) {
     view = Summary
   } else if (/\/ingredients\/([a-z0-9-]+)/i.test(pathname)) {
     view = IngredientDetail
+  }
+
+  if (!locationListener) {
+    locationListener = () => actions.updatePathname(window.location.pathname)
+    window.addEventListener('popstate', locationListener);
   }
 
   return view(state, actions)
