3a7e663ef59dc014c00cc80731575c5260d1e240
Pablo Molina <contact@pablomolina.me>
2018-11-17 12:23:27 +0100

    Add summary

--- client/components/summary.js
+++ client/components/summary.js
@@ -3,6 +3,11 @@ import Link, { goBack } from './link';
 import backArrow from '../img/back.png'
 
 function Summary (state, actions) {
+  if (state.ingredients && !state.ingredients.length) {
+    actions.loadIngredients()
+    return ''
+  }
+
   return <div>
     <div class="header">
       <Link actions={actions} href={goBack}>
@@ -10,6 +15,35 @@ function Summary (state, actions) {
       </Link>
 
       <span>POKE</span><span class="header-si">SI</span>
+    </div>
+
+    <div class="ingredients">
+      <p class="ingredients__intro">Tu resumen:</p>
+
+      <ul class="ingredients-list ingredient-list--summary">{
+        state.ingredients
+          .filter(ingredient => state.ingredientsSummary[ingredient.slug])
+          .map(ingredient =>
+        <li class="ingredient-item">
+          <Link actions={actions} class="ingredient__link" href={`/ingredients/${ingredient.slug}`}>
+            <div class="ingredient-cell">
+              <span class="ingredient__name">{ingredient.name}</span>
+              <span class="ingredient__weight">{ingredient.weight}g</span>
+            </div>
+          </Link>
+          <div class="ingredient-quantity">
+            <button class="quantity__minus" type="button" onclick={() => actions.removeIngredient(ingredient.slug)}>-</button>
+            <span class="quantity__total">{state.ingredientsSummary[ingredient.slug] || 0}</span>
+            <button class="quantity__more" type="button" onclick={() => actions.addIngredient(ingredient.slug)}>+</button>
+          </div>
+        </li>
+      )}</ul>
+
+      <div class="summary-total">
+        <div class="summary-total__amount">Total: 9,95€</div>
+        <button class="resume__link" type="button">Realizar pedido</button>
+      </div>
+
     </div>
   </div>
 }
