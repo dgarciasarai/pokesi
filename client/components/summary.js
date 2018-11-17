cfbc20cefd697cd400222a9601325bf66fc7ed71
Pablo Molina <contact@pablomolina.me>
2018-11-17 11:53:11 +0100

    Update styles

--- client/components/summary.js
+++ client/components/summary.js
@@ -1,7 +1,17 @@
 import { h } from 'hyperapp'
+import Link, { goBack } from './link';
+import backArrow from '../img/back.png'
 
 function Summary (state, actions) {
-  return <div>Summary</div>
+  return <div>
+    <div class="header">
+      <Link actions={actions} href={goBack}>
+        <img alt="Volver" src={backArrow} class="header-detail-back-arrow"/>
+      </Link>
+
+      <span>POKE</span><span class="header-si">SI</span>
+    </div>
+  </div>
 }
 
 export { Summary }
