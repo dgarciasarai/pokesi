68d13057b1c63f9a514b25dc5cc702dd827c7df5
Pablo Molina <contact@pablomolina.me>
2018-11-18 20:17:43 +0100

    Notification: sendNotification

--- client/to-do/notification.js
+++ client/to-do/notification.js
@@ -10,6 +10,36 @@ function setServiceWorkerRegistration (_serviceWorkerRegistration) {
  * @param {string} body
  */
 function sendNotification(title, body) {
+  if ('Notification' in global) {
+    if (Notification.permission === 'granted') {
+      const options = {
+        body,
+        tag: 'poke-ready',
+        lang: 'es',
+        badge: '/img/favicon/16x16.png',
+        icon: '/img/favicon/192x192.png',
+        vibrate: [200],
+        renotify: true,
+        requireInteraction: true
+      }
+
+      if (serviceWorkerRegistration) {
+        options.actions = [
+          { action: 'mesa', title: 'Traer a la mesa' }
+        ]
+
+        serviceWorkerRegistration.showNotification(title, options)
+      } else {
+        new Notification(title, options)
+      }
+    } else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(permission => {
+        if (permission === 'granted') {
+          sendNotification(title, body)
+        }
+      })
+    }
+  }
 }
 
 export {
