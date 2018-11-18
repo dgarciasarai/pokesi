d2d69c6d18acee1443df26a4321f1aef4ce5d008
Pablo Molina <contact@pablomolina.me>
2018-11-18 20:15:51 +0100

    ServiceWorker: respondWithCachedContent

--- client/to-do/fetch.js
+++ client/to-do/fetch.js
@@ -6,6 +6,29 @@ import { isClientURL } from '../utils/is-app-url'
  * @param {string} cacheVersion
  */
 async function respondWithCachedContent (event, cacheVersion) {
+  const url = event.request.url
+
+  const cache = await caches.open(cacheVersion)
+  let response = await cache.match(url)
+
+  if (!response) {
+    try {
+      response = await fetch(url)
+    } catch (error) {
+      console.error(error)
+      console.error(url)
+
+      if (isClientURL(url)) {
+        console.log(`Using index.html for ${url}`)
+        response = cache.match('/index.html')
+      } else {
+        console.log(`Using 404.html for ${url}`)
+        response = cache.match('/404.html')
+      }
+    }
+  }
+
+  return response
 }
 
 export {
